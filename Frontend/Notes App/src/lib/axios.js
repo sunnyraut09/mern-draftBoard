588d08628c1d192fc9bef3131a304604fba861df
Sunny Raut <sunnyraut170@gmail.com>
2026-02-03 23:35:36 +0530

    fix applied correct baseURL variable

--- Frontend/Notes App/src/lib/axios.js
+++ Frontend/Notes App/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 const axiosapi = axios.create(
     {
-        baseURL : "http://localhost:5001/api"
+        baseURL : BASE_URL
     }
 )
 
